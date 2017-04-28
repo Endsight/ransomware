f562939c5e3df159442861b76f4b48d7bae0742f
csmithendsight <csmith@endsight.net>
2017-04-28 11:25:36 -0700

    Update ransomwareFSRM.ps1

--- ransomwareFSRM.ps1
+++ ransomwareFSRM.ps1
@@ -1,10 +1,10 @@
-$decryptreadme = (Invoke-WebRequest "https://raw.githubusercontent.com/csmithendsight/ransomware/readme.txt").Content
+$decryptreadme = (Invoke-WebRequest "https://raw.githubusercontent.com/csmithendsight/ransomware/master/readme.txt").Content
 
-$fileexts = (Invoke-WebRequest "https://raw.githubusercontent.com/csmithendsight/ransomware/extensions.txt").Content
+$fileexts = (Invoke-WebRequest "https://raw.githubusercontent.com/csmithendsight/ransomware/master/extensions.txt").Content
 
 $filescreengroup = @()
 
 foreach($line in $decryptreadme.Split("`r`n")){ if ($line -ne "") {$filescreengroup += $line} }
   foreach($line in $fileexts.Split("`r`n")){ if ($line -ne "") {$filescreengroup += $line} }
 
-Get-FsrmFileGroup "CryptowareDEV" | Set-FsrmFileGroup -IncludePattern $filescreengroup
+Get-FsrmFileGroup "CryptoDev" | Set-FsrmFileGroup -IncludePattern $filescreengroup
